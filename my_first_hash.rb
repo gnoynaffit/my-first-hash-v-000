82a1e493b666b7ed324845f13b8bc1b07a72dd90
Tiffany <ong.tifffany1503@gmail.com>
2019-07-22 20:23:09 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -33,12 +33,12 @@ def adding
                   "gun powder" => 4
                  }
   # add 2 muskets to the shipping_manifest hash below
-  shipping_manifest["muskets"]
+  shipping_manifest["muskets"] = 2
 
 
 
   # add 4 gun powder to the shipping_manifest hash below
-  shipping_manifest["gun powder"]
+  shipping_manifest["gun powder"] = 4
 
 
 
